eab25befa8786bf0eda1761a3381d5920339a1dd
清水 美里 <mi-shimizu@sakura.ad.jp>
2019-08-23 19:06:03 +0900

    created a script set of benchmark

---
+++ benchmark.bash
@@ -0,0 +1,22 @@
+#!/bin/bash -xu
+
+dir=$(cd $(dirname $0); pwd)
+cpu_dir=${dir}/unixbench
+mem_dir=${dir}/STREAM
+st_dir=${dir}/fio
+db_dir=${dir}/sysbench
+
+# --- CPU section ---
+${cpu_dir}/Run
+
+# --- MEMORY section ---
+mem_log="${mem_dir}/results/stream_`date +%Y%m%d%H%M`.log"
+${mem_dir}/stream > ${mem_log}
+
+# --- STORAGE section ---
+fio_file="${st_dir}/bench.ini"
+fio_log="${st_dir}/results/fio_`date +%Y%m%d%H%M`.log"
+fio ${fio_file} > ${fio_log}
+
+# --- DB section ---
+${db_dir}/run.sh
