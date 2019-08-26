16eb1b1523299b2b8406e48d8228700e0c70187d
清水 美里 <mi-shimizu@sakura.ad.jp>
2019-08-26 10:31:19 +0900

    change the way to execute

--- benchmark.bash
+++ benchmark.bash
@@ -7,17 +7,21 @@ st_dir=${dir}/fio
 db_dir=${dir}/sysbench
 
 # --- CPU section ---
-${cpu_dir}/Run
+cd ${cpu_dir}
+./Run
 
 # --- MEMORY section ---
 mem_log="${mem_dir}/results/stream_`date +%Y%m%d%H%M`.log"
-${mem_dir}/stream > ${mem_log}
+cd ${mem_dir}
+./stream > ${mem_log}
 
 # --- STORAGE section ---
-fio_file="${st_dir}/bench.ini"
-fio_log="${st_dir}/results/fio_`date +%Y%m%d%H%M`.log"
+cd ${st_dir}
+fio_file="bench.ini"
+fio_log="results/fio_`date +%Y%m%d%H%M`.log"
 fio ${fio_file} > ${fio_log}
 
 # --- DB section ---
-${db_dir}/run.sh
+cd ${db_dir}
+./run.sh
 
